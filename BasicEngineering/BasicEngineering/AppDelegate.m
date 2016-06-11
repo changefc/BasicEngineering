5a03fee3f6faecf9cf76f2f07f89d43144dc8d14
tusm <wq\ngit commit -m fixed>
2016-06-11 23:40:25 +0800

    完成空工程构建

--- BasicEngineering/BasicEngineering/AppDelegate.m
+++ BasicEngineering/BasicEngineering/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window=[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    ViewController *viewController=[[ViewController alloc] init];
+    [self.window setRootViewController:viewController];
+    
     return YES;
 }
 
