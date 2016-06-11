25a9fa4b8330d51bd8d65bc46ecb9c020cf76c56
tusm <wq\ngit commit -m fixed>
2016-06-11 22:25:15 +0800

    1:初始化空工程

---
+++ BasicEngineering/BasicEngineering/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BasicEngineering
+//
+//  Created by tusm on 16/6/11.
+//  Copyright © 2016年 zouxianfc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
